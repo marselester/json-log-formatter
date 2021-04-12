f13a89c5ed465130f3198063e7ba9c10e436d63c
Robert Reiz <robert.reiz.81@gmail.com>
2021-04-12 16:55:58 +0200

    Add license information to the setup.py
    Tools like VersionEye are using this kind of information for license compliance analysis.

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from distutils.core import setup
 setup(
     name='JSON-log-formatter',
     version='0.3.0',
+    license="MIT",
     packages=['json_log_formatter'],
     author='Marsel Mavletkulov',
     author_email='marselester@ya.ru',
